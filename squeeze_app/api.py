c271c05d4349120f8f0412ab48109209dc60ca83
wilsonzhen <wilsonzhen@gmail.com>
2018-08-04 14:00:46 -0400

    added functionality for post and put

--- squeeze_app/api.py
+++ squeeze_app/api.py
@@ -18,27 +18,78 @@ PORT = int(sys.argv[2])
 DBNAME = sys.argv[3]
 USER = sys.argv[4]
 
-# To be deleted
-users = {
-	1: {'id': 1,
-		'first_name': 'Wilson',
-		'last_name': 'Zhen',
-		'email': 'wilson@gmail.com',
-		'created_at': datetime(2018,7,28).strftime('%Y-%m-%d'),
-		'updated_at': datetime(2018,7,28).strftime('%Y-%m-%d'),
-	},
-	2: {'id': 2,
-		'first_name': 'Jun',
-		'last_name': 'Chen',
-		'email': 'jun@gmail.com',
-		'created_at': datetime(2018,7,28).strftime('%Y-%m-%d'),
-		'updated_at': datetime(2018,7,28).strftime('%Y-%m-%d'),
-	},
-}
 
 @app.route('/')
 def index():
-	return 'Welcome to the Squeeze App API\n'
+    return 'Welcome to the Squeeze App API\n'
+
+
+class UserList(Resource):
+    def get(self):
+        conn = pg2.connect(host=HOST, port=PORT, dbname=DBNAME, user=USER)
+        cur = conn.cursor(cursor_factory=pg2_extras.DictCursor)        
+
+        cur.execute('''
+            SELECT user_id, username, email, first_name, last_name, created_on
+            from users
+            order by user_id
+        '''
+        )
+
+        users = cur.fetchall()
+        
+        cur.close()
+        conn.close()
+
+        if users:
+            return [
+                {
+                    'user_id': user['user_id'],
+                    'username': user['username'],
+                    'email': user['email'],
+                    'first_name': user['first_name'],
+                    'last_name': user['last_name'],
+                    'created_on': user['created_on'].timestamp(), # convert datetime to epoch
+                } 
+                for user in users
+            ], 200
+        else:
+            return {'error_msg': 'no users :('}, 404
+
+    def post(self):
+        conn = pg2.connect(host=HOST, port=PORT, dbname=DBNAME, user=USER)
+        cur = conn.cursor(cursor_factory=pg2_extras.DictCursor)
+        
+        if request.form:
+            data = request.form
+        elif request.json:
+            data = request.json
+
+        current_time = datetime.now()
+
+        cur.execute(
+            ''' INSERT INTO users (username, password, email, first_name, last_name, created_on, updated_on)
+                VALUES (%(username)s, %(password)s, %(email)s, %(first_name)s, %(last_name)s, %(created_on)s, %(updated_on)s)
+                RETURNING user_id;
+            ''',
+            {
+                'username': data.get('username'),
+                'password': data.get('password'),
+                'email': data.get('email'),
+                'first_name': data.get('first_name'),
+                'last_name': data.get('last_name'),
+                'created_on': current_time,
+                'updated_on': current_time,
+            }
+        )
+
+        user_id = cur.fetchone()['user_id']
+        
+        conn.commit()
+        cur.close()
+        conn.close()        
+
+        return self.get()
 
 
 class User(Resource):
@@ -47,9 +98,9 @@ class User(Resource):
         cur = conn.cursor(cursor_factory=pg2_extras.DictCursor)
         
         cur.execute('''
-        		select user_id, username, email, first_name, last_name, created_on
-        		from users
-        		where user_id = {}'''.format(user_id))
+            SELECT user_id, username, email, first_name, last_name, created_on
+            from users
+            where user_id = {}'''.format(user_id))
 
         user_data = cur.fetchone()
         
@@ -57,26 +108,71 @@ class User(Resource):
         conn.close()
 
         if user_data is None:
-        	return {'error_msg': 'user not found'}, 404
+            return {'error_msg': 'user not found'}, 404
         else:
-	        return {
-	        	'user_id': user_data['user_id'],
-	        	'username': user_data['username'],
-	        	'email': user_data['email'],
-	        	'first_name': user_data['first_name'],
-	        	'last_name': user_data['last_name'],
-	        	'created_on': user_data['created_on'].timestamp(), # convert datetime to epoch
-	        }
-        
-        # return {'hi': 'bro'},
-        # return users.get(user_id)
+            return {
+                'user_id': user_data['user_id'],
+                'username': user_data['username'],
+                'email': user_data['email'],
+                'first_name': user_data['first_name'],
+                'last_name': user_data['last_name'],
+                'created_on': user_data['created_on'].timestamp(), # convert datetime to epoch
+            }, 200
+
 
     def put(self, user_id):
-    	ipdb.set_trace()
-    	users[user_id] = request.form['data']
-    	return {user_id: users[user_id]}
+        conn = pg2.connect(host=HOST, port=PORT, dbname=DBNAME, user=USER)
+        cur = conn.cursor(cursor_factory=pg2_extras.DictCursor)
+
+        if request.form:
+            data = request.form
+        elif request.json:
+            data = request.json
+        
+        current_data, response_code = self.get(user_id)
+        current_time = datetime.now()
+
+        #If the user doesn't exist, create the user. If the user does exist, update the user.        
+        if response_code == 404:
+            cur.execute(
+                ''' INSERT INTO users (user_id, username, password, email, first_name, last_name, created_on, updated_on)
+                    VALUES (%(user_id)s, %(username)s, %(password)s, %(email)s, %(first_name)s, %(last_name)s, %(created_on)s, %(updated_on)s)
+                ''',
+                {
+                    'user_id': user_id,
+                    'username': data.get('username'),
+                    'password': data.get('password'),
+                    'email': data.get('email'),
+                    'first_name': data.get('first_name'),
+                    'last_name': data.get('last_name'),
+                    'created_on': current_time,
+                    'updated_on': current_time,
+                }
+            )            
+        elif response_code == 200:
+            cur.execute(
+                '''UPDATE users SET (username, email, first_name, last_name, updated_on) = 
+                    (%(username)s, %(email)s, %(first_name)s, %(last_name)s, %(updated_on)s)
+                    WHERE user_id = %(user_id)s
+                ''', 
+                {
+                    'user_id': user_id,
+                    'username': data.get('username', current_data['username']),
+                    'email': data.get('email', current_data['email']),
+                    'first_name': data.get('first_name', current_data['first_name']),
+                    'last_name': data.get('last_name', current_data['last_name']),
+                    'updated_on': current_time,
+                }
+            )
+        
+        conn.commit()
+        cur.close()
+        conn.close()
+        
+        return self.get(user_id)
 
 
+api.add_resource(UserList, '/user')
 api.add_resource(User, '/user/<int:user_id>')
 
 
